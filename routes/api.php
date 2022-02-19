9974733cb08e8d555c49feea5d0e7990e0b8c2a9
青塚琢也 <takuya.aotsuka@aotsukatakuyanoMacBook-Air.local>
2022-02-19 13:39:31 +0900

    add routing for health check.

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group(['prefix' => 'health'], function() {
+    Route::get('', function() {
+        return [
+            'health' => true,
+        ];
+    });
+});
+
 Route::group(['prefix' => 'categories'], function() {
     Route::get('', [CategoryController::class, 'index'])->name('category.index');
     Route::get('/{id}', [CategoryController::class, 'find'])->name('category.find');
